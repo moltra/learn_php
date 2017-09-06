d079fa67e7c4ad50f6c2126884d35a075ce2e806
Stephen  strickland <smsmail@roadrunner.com>
2017-09-06 06:13:37 +0000

    added links to index.php

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     </head>
     <body>
         <?php
-            echo "<p>Hello World!</p>";
+            echo '<p>Hello World!</p>';
         ?>
+        <a href="login.php"> Click here to login</a>
+        <a href="register.php"> Click here to register
     </body>
 </html>

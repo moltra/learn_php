9034e28e12a0ceaa74e9bfade18cd8fdf506ea28
Stephen  strickland <smsmail@roadrunner.com>
2017-09-06 06:05:44 +0000

    started php tutorials for beginners

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My first PHP Webstite</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hello World!</p>";
+        ?>
+    </body>
+</html>
